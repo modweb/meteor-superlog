96acfbeb01f208288b246374bba933b4a9f6d401
Justin Foley <justin@modweb.io>
2015-11-20 12:16:58 -0500

    include momentjs

--- package.js
+++ package.js
@@ -1,19 +1,19 @@
 Package.describe({
   name: 'modweb:superlog',
   summary: 'Timestamp logs.',
-  version: '0.1.1',
+  version: '0.1.2_1',
   git: 'https://github.com/modweb/meteor-superlog'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1');
-  api.use(['coffeescript']);
+  api.use(['coffeescript', 'momentjs:moment@2.8.4']);
   api.addFiles('superlog.litcoffee', ['client', 'server']);
   api.export('Superlog');
 });
 
 Package.onTest(function(api) {
-  api.use(['tinytest', 'test-helpers', 'coffeescript', 'http']);
+  api.use(['tinytest', 'test-helpers', 'coffeescript', 'momentjs:moment@2.8.4']);
   api.use('modweb:superlog');
   api.addFiles('tests/superlog-tests.litcoffee');
 });
